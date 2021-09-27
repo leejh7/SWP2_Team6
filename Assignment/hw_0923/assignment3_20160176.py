002c598fb02364b6ab6068644040c5180ae7094d
youngjin96 <61217641+youngjin96@users.noreply.github.com>
2021-09-28 02:08:10 +0900

    Update assignment3_20160176.py

--- Assignment/hw_0923/assignment3_20160176.py
+++ Assignment/hw_0923/assignment3_20160176.py
@@ -1,3 +1,98 @@
 import pickle
 
-dbfilename = 'assignment3_20160176.dat'
+dbfilename = 'test3_4.dat'
+
+def readScoreDB():
+    try:
+        fH = open(dbfilename, 'rb')
+    except FileNotFoundError as e:
+        print("New DB: ", dbfilename)
+        return []
+
+    scdb = []
+    try:
+        scdb =  pickle.load(fH)
+        for i in scdb:
+            i['Age'] = int(i['Age'])
+            i['Score'] = int(i['Score'])
+        print(scdb)
+    except:
+        print("Empty DB: ", dbfilename)
+    else:
+        print("Open DB: ", dbfilename)
+    fH.close()
+    return scdb
+
+
+# write the data into person db
+def writeScoreDB(scdb):
+    fH = open(dbfilename, 'wb')
+    pickle.dump(scdb, fH)
+    fH.close()
+
+
+def doScoreDB(scdb):
+    while(True):
+        try:
+            inputstr = (input("Score DB > "))
+            if inputstr == "":
+                continue
+            parse = inputstr.split(" ")
+            if parse[0] == 'add':
+                if len(parse) < 4:
+                    print('name, age, score를 모두 입력하세요.')
+                    continue
+                if not (parse[2].isdigit() and parse[3].isdigit()):
+                    print('age와 score는 숫자로 입력하세요.')
+                    continue
+                record = {'Name': parse[1], 'Age': int(parse[2]), 'Score': int(parse[3])}
+                scdb += [record]
+            elif parse[0] == 'del':
+                if len(parse) < 2:
+                    print('이름을 입력해주세요')
+                    continue
+                for p in scdb:
+                    if p['Name'] == parse[1]:
+                        scdb.remove(p)
+                        break
+            elif parse[0] == 'show':
+                sortKey = 'Name' if len(parse) == 1 else parse[1]
+                showScoreDB(scdb, sortKey)
+            elif parse[0] == 'quit':
+                break
+            elif parse[0] == 'find':
+                if len(parse) < 2:
+                    print('이름을 입력해주세요')
+                    continue
+                tmp_scdb = []
+                for i in scdb:
+                    if i['Name'] == parse[1]:
+                        tmp_scdb.append(i)
+                        showScoreDB(tmp_scdb, 'Name')
+                        break
+            elif parse[0] == 'inc':
+                if len(parse) < 3:
+                    print('name과 amount를 모두 입력하세요.')
+                    continue
+                if not parse[2].isdigit():
+                    print('amount는 숫자로 입력하세요.')
+                    continue
+                for i in scdb:
+                    if i['Name'] == parse[1]:
+                        i['Score'] += int(parse[2])
+            else:
+                print("Invalid command: " + parse[0])
+        except Exception as e:
+            print(e)
+
+
+def showScoreDB(scdb, keyname):
+    for p in sorted(scdb, key=lambda person: person[keyname]):
+        for attr in sorted(p):
+            print(attr + "=" + str(p[attr]), end=' ')
+        print()
+
+
+scoredb = readScoreDB()
+doScoreDB(scoredb)
+writeScoreDB(scoredb)
