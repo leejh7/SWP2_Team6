1aeaa0c3a1afd8f275ee8e84a6e1fbf2d7a5376d
hayeong-lee <64948651+hayeong-lee@users.noreply.github.com>
2021-10-11 17:29:24 +0900

    Update assignment2_20185290.py

---
+++ Assignment/hw_1007/assn2/assignment2_20185290.py
@@ -0,0 +1,65 @@
+import time
+import random
+
+
+#이진탐색
+def seqsearch(nbrs, target):
+    for i in range(0, len(nbrs)):
+        if target == nbrs[i]:
+            return i
+    return -1
+
+#이진탐색-재귀
+def recbinsearch(L, l, u, target):
+  if l>u:
+    return -1
+  elif l==u:
+    if L[l]==target:
+      return l
+    else:
+      return -1
+  else:
+    mid=(l+u)//2
+    if L[mid]==target:
+      return mid
+    elif L[mid]>target:
+      return recbinsearch(L,l,mid-1,target)
+    else:
+      return recbinsearch(L,mid+1,u,target)
+  
+
+
+numofnbrs = int(input("Enter a number: "))
+numbers = []
+for i in range(numofnbrs):
+    numbers += [random.randint(0, 999999)]
+
+numbers = sorted(numbers)
+
+numoftargets = int(input("Enter the number of targets: "))
+targets = []
+for i in range(numoftargets):
+    targets += [random.randint(0, 999999)]
+
+
+ts = time.time()
+
+# binary search - recursive
+cnt = 0
+for target in targets:
+    idx = recbinsearch(numbers, 0, len(numbers), target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("recbinsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
+
+ts = time.time()
+
+# sequential search
+cnt = 0
+for target in targets:
+    idx = seqsearch(numbers, target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("seqsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
