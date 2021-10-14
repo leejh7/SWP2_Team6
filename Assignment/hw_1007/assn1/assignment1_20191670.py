2571c307646512e6de088aa1bb07f2fc2d6894c7
NaYoung2253 <jny2253@kookmin.ac.kr>
2021-10-14 12:06:14 +0900

    hw1007

---
+++ Assignment/hw_1007/assn1/assignment1_20191670.py
@@ -0,0 +1,34 @@
+import time
+
+def fibo(n): # 재귀적
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fibo(n - 1) + fibo(n - 2)
+
+def iterfibo(n): # 반복
+    if n <= 2:
+        return 1
+
+    a, b = 0, 1
+
+    for x in range(n - 1):
+        a, b = b, a + b
+    return b
+
+while True:
+    nbr = int(input("Enter a number: "))
+    if nbr == -1:
+        break
+
+    ts = time.time()
+    fibonumber = iterfibo(nbr)
+    ts = time.time() - ts
+    print("Fibo(%d)=%d, time %.6f" % (nbr, fibonumber, ts))
+
+    ts = time.time()
+    fibonumber = fibo(nbr)
+    ts = time.time() - ts
+    print("IterFibo(%d)=%d, time %.6f" % (nbr, fibonumber, ts))
